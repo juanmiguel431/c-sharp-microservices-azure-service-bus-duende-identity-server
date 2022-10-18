533f6ec6192dfe49dd509d80276ae9b57c949eaf
juanmiguel431 <juanmiguel431@gmail.com>
2022-10-17 23:09:04 -0400

    Database created.

---
+++ Mango.Services.ProductApi/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Mango.Services.ProductApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.ProductApi.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
