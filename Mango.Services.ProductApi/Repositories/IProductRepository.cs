62a0900b2d75c2a3bc426efbbeb73e349dae67dc
juanmiguel431 <juanmiguel431@gmail.com>
2022-10-19 19:53:20 -0400

    Some Dtos and IProductRepository interface

---
+++ Mango.Services.ProductApi/Repositories/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using Mango.Services.ProductApi.Models.Dtos;
+
+namespace Mango.Services.ProductApi.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductDto>> GetProducts();
+        Task<ProductDto> GetProductById(int productId);
+        Task<ProductDto> CreateUpdateProduct(ProductDto productDto);
+        Task<bool> DeleteProduct(int productId);
+    }
+}
