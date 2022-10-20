9730543ee3d9ace9bb6147d9f6fa425c4a8ab66f
juanmiguel431 <juanmiguel431@gmail.com>
2022-10-19 21:35:20 -0400

    ProductApiController added

---
+++ Mango.Services.ProductApi/Controllers/ProductApiController.cs
@@ -0,0 +1,52 @@
+﻿using Mango.Services.ProductApi.Models.Dtos;
+using Mango.Services.ProductApi.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ProductApi.Controllers
+{
+    [Route("api/products")]
+    public class ProductApiController : Controller
+    {
+
+        protected ResponseDto _response;
+        private readonly IProductRepository _productRepository;
+
+        public ProductApiController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+            _response = new ResponseDto();
+        }
+
+        [HttpGet]
+        public async Task<ResponseDto> Get()
+        {
+            try
+            {
+                IEnumerable<ProductDto> productDtos = await _productRepository.GetProducts();
+                _response.Result = productDtos;
+            }
+            catch (Exception e)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { e.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpGet, Route("{id}")]
+        public async Task<ResponseDto> Get(int id)
+        {
+            try
+            {
+                var productDto = await _productRepository.GetProductById(id);
+                _response.Result = productDto;
+            }
+            catch (Exception e)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { e.ToString() };
+            }
+            return _response;
+        }
+    }
+}
--- Mango.Services.ProductApi/Models/Dtos/ResponseDto.cs
+++ Mango.Services.ProductApi/Models/Dtos/ResponseDto.cs
@@ -3,8 +3,8 @@
     public class ResponseDto
     {
         public bool IsSuccess { get; set; } = true;
-        public object Result { get; set; }
+        public object? Result { get; set; }
         public string DisplayMessage { get; set; } = string.Empty;
-        public List<string> ErrorMessages { get; set; }
+        public List<string>? ErrorMessages { get; set; }
     }
 }
