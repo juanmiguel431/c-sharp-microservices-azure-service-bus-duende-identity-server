778146febf7bd624f495cf4867e53bcc3bfcdf11
juanmiguel431 <juanmiguel431@gmail.com>
2022-10-22 23:33:08 -0400

    Product Controller added.

---
+++ Mango.Web/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Mango.Web.Controllers;
+
+public class ProductController : Controller
+{
+    private readonly IProductService _productService;
+
+    public ProductController(IProductService productService)
+    {
+        _productService = productService;
+    }
+    
+    public async Task<IActionResult> ProductIndex()
+    {
+        List<ProductDto> list;
+        var response = await _productService.GetAllProductsAsync<ResponseDto>();
+        if (response != null && response.IsSuccess)
+        {
+            list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+        }
+        else
+        {
+            list = new List<ProductDto>();
+        }
+        return View(list);
+    }
+}
