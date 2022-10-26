fd972a3aa59abc8077c2d390d1609f27f52d2341
juanmiguel431 <juanmiguel431@gmail.com>
2022-10-26 17:33:11 -0400

    AddFirstLasNameToUser migration added.

--- Mango.Services.Identity/Models/ApplicationUser.cs
+++ Mango.Services.Identity/Models/ApplicationUser.cs
@@ -4,5 +4,6 @@ namespace Mango.Services.Identity.Models;
 
 public class ApplicationUser : IdentityUser
 {
-    
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
 }
