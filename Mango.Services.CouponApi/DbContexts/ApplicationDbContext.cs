0015b7bcaf8515e7813198124968cda467d3cc4e
juanmiguel431 <juanmiguel431@gmail.com>
2022-10-30 00:09:55 -0400

    Coupon Api project setup

---
+++ Mango.Services.CouponApi/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Mango.Services.CouponApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.CouponApi.DbContexts;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Coupon> Coupons { get; set; }
+}
---
+++ Mango.Services.CouponApi/MappingConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Mango.Services.CouponApi.Models;
+using Mango.Services.CouponApi.Models.Dtos;
+
+namespace Mango.Services.CouponApi;
+
+public class MappingConfig
+{
+    public static MapperConfiguration RegisterMaps()
+    {
+        var mappingConfig = new MapperConfiguration(config =>
+        {
+            config.CreateMap<CouponDto, Coupon>().ReverseMap();
+        });
+
+        return mappingConfig;
+    }
+}
