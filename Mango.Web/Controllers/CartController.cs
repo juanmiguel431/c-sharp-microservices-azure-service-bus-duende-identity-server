d9f957fc4ea619a6442db5a6f01162e9bf6623d9
juanmiguel431 <juanmiguel431@gmail.com>
2022-10-29 22:52:05 -0400

    Cart View added.

---
+++ Mango.Web/Controllers/CartController.cs
@@ -0,0 +1,67 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Services.IServices;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Mango.Web.Controllers;
+
+public class CartController : Controller
+{
+    private readonly IProductService _productService;
+    private readonly ICartService _cartService;
+    private const string AccessToken = "access_token";
+
+    public CartController(IProductService productService, ICartService cartService)
+    {
+        _productService = productService;
+        _cartService = cartService;
+    }
+
+    [Authorize]
+    public async Task<IActionResult> CardIndex()
+    {
+        var cartDto = await LoadCartDto();
+        return View(cartDto);
+    }
+
+    private async Task<CartDto> LoadCartDto()
+    {
+        var userId = GetUserId();
+        var token = await GetToken();
+
+        var response = await _cartService.GetCartByUserIdAsync<ResponseDto>(userId, token);
+
+        CartDto? cartDto;
+        if (response != null && response.IsSuccess)
+        {
+            cartDto = JsonConvert.DeserializeObject<CartDto>(Convert.ToString(response.Result));
+        }
+        else
+        {
+            cartDto = new CartDto();
+        }
+
+        if (cartDto.CartHeader != null)
+        {
+            foreach (var detail in cartDto.CartDetails)
+            {
+                cartDto.CartHeader.OrderTotal += (detail.Product.Price * detail.Count);
+            }
+        }
+
+        return cartDto;
+    }
+    
+    private string GetUserId()
+    {
+        var claim = User.Claims.Single(u => u.Type == "sub");
+        return claim.Value;
+    }
+    
+    private async Task<string?> GetToken()
+    {
+        return await HttpContext.GetTokenAsync(AccessToken);
+    }
+}
