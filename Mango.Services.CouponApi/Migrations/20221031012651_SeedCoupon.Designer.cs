6bb47c48d6e108a5ef8fcfc3be450eda48d67452
juanmiguel431 <juanmiguel431@gmail.com>
2022-10-30 21:28:14 -0400

    Seeding the coupon table.

--- Mango.Services.CouponApi/Migrations/20221031012651_SeedCoupon.Designer.cs
+++ Mango.Services.CouponApi/Migrations/20221031012651_SeedCoupon.Designer.cs
@@ -3,6 +3,7 @@ using Mango.Services.CouponApi.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Mango.Services.CouponApi.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221031012651_SeedCoupon")]
+    partial class SeedCoupon
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
