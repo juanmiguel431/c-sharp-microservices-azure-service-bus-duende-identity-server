bb2789dd538059bff86bc4236a74fa88e61d4699
juanmiguel431 <juanmiguel431@gmail.com>
2022-10-30 21:27:48 -0400

    ICouponRepository added

---
+++ Mango.Services.CouponApi/Repositories/ICouponRepository.cs
@@ -0,0 +1,8 @@
+﻿using Mango.Services.CouponApi.Models.Dtos;
+
+namespace Mango.Services.CouponApi.Repositories;
+
+public interface ICouponRepository
+{
+    Task<CouponDto> GetCouponByCode(string couponCode);
+}
