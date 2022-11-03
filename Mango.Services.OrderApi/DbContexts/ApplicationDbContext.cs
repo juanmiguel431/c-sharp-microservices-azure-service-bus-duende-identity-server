d5ab6f987483bbbdb5d404e0b3ca37c13ea05b93
juanmiguel431 <juanmiguel431@gmail.com>
2022-11-02 20:45:55 -0400

    Models added to Order Api

---
+++ Mango.Services.OrderApi/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Mango.Services.OrderApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.OrderApi.DbContexts;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<OrderHeader> OrderHeaders { get; set; }
+    public DbSet<OrderDetail> OrderDetails { get; set; }
+}
---
+++ Mango.Services.OrderApi/Models/OrderDetail.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mango.Services.OrderApi.Models;
+
+public class OrderDetail
+{
+    [Key]
+    public int OrderDetailId { get; set; }
+    public int OrderHeaderId { get; set; }
+    
+    [ForeignKey(nameof(OrderHeaderId))]
+    public virtual OrderHeader OrderHeader { get; set; }
+    
+    public int ProductId { get; set; }
+    public int Count { get; set; }
+    public string ProductName { get; set; }
+    public double Price { get; set; }
+}
