9a371354bb4720db21c7b402984c4a4759caabf0
juanmiguel431 <juanmiguel431@gmail.com>
2022-11-02 22:43:41 -0400

    OrderApi - Program setup added.

---
+++ Mango.Services.OrderApi/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+
+namespace Mango.Services.OrderApi;
+
+public class MappingConfig
+{
+    public static MapperConfiguration RegisterMaps()
+    {
+        var mappingConfig = new MapperConfiguration(config =>
+        {
+            // config.CreateMap<ProductDto, Product>().ReverseMap();
+            // config.CreateMap<CartHeaderDto, CartHeader>().ReverseMap();
+            // config.CreateMap<CartDetailDto, CartDetail>().ReverseMap();
+            // config.CreateMap<CartDto, Cart>().ReverseMap();
+        });
+
+        return mappingConfig;
+    }
+}
--- Mango.Services.OrderApi/Program.cs
+++ Mango.Services.OrderApi/Program.cs
@@ -1,3 +1,9 @@
+using Mango.Services.OrderApi;
+using Mango.Services.OrderApi.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +11,61 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo() { Title = "Mango.services.ShoppingCartApi", Description = "Shopping Cart Api", Version = "V1"});
+    c.EnableAnnotations();
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Description = $"Enter 'Bearer' [space] and your token",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    
+    var openApiSecurityScheme = new OpenApiSecurityScheme
+    {
+        Reference = new OpenApiReference
+        {
+            Type = ReferenceType.SecurityScheme,
+            Id = "Bearer"
+        },
+        Scheme = "OAuth2",
+        Name = "Bearer",
+        In = ParameterLocation.Header
+    };
+    
+    var openApiSecurityRequirement = new OpenApiSecurityRequirement { { openApiSecurityScheme, new List<string>() } };
+    
+    c.AddSecurityRequirement(openApiSecurityRequirement);
+});
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddAuthentication("Bearer").AddJwtBearer("Bearer", options =>
+{
+    options.Authority = builder.Configuration["ServiceUrls:IdentityApi"];
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateAudience = false,
+    };
+});
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("ApiScope", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("scope", "mango");
+    });
+});
+
+var mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+// builder.Services.AddScoped<ICartRepository, CartRepository>();
+// builder.Services.AddSingleton<IMessageBus, AzureServiceBusMessageBus>();
 
 var app = builder.Build();
 
@@ -17,7 +77,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
