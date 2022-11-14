2d9e6378ff646269d082a0a4cb1166abf12ac900
juanmiguel431 <juanmiguel431@gmail.com>
2022-11-14 15:24:39 -0500

    Ocelot route configuration added.

--- Mango.Gateway.Solution/Program.cs
+++ Mango.Gateway.Solution/Program.cs
@@ -1,6 +1,22 @@
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication("Bearer").AddJwtBearer("Bearer", options =>
+{
+    options.Authority = builder.Configuration["ServiceUrls:IdentityApi"];
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateAudience = false,
+    };
+});
+
+builder.Services.AddOcelot();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+await app.UseOcelot();
 
 app.Run();
